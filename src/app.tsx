ad4e8bdaa3bf4cfcec085fd071bfa923ad599ee9
Steve Konves <stephen.konves@gmail.com>
2020-12-15 11:27:52 -0700

    Revert text

--- src/app.tsx
+++ src/app.tsx
@@ -65,7 +65,7 @@ const Uploader: React.FC = () => {
 
       <p>
         To get a schema file, run the Apollo introspection query. Save the
-        results and drop the file onto this box.
+        results and drag the file into this box.
       </p>
     </div>
   );
