c00bb52a27ab22ba3cda8b8a885f99e7ee0c2b56
Steve Konves <stephen.konves@gmail.com>
2020-12-20 17:45:18 -0700

    Upgrade domain-graph

--- src/app.tsx
+++ src/app.tsx
@@ -1,58 +1,38 @@
 import './app.less';
 
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useRef } from 'react';
 
-import UploadCloud from 'domain-graph/lib/icons/upload-cloud';
-type Schema = DomainGraphProps['schema'];
+import {
+  BrowserOpenFileDialog,
+  DataProvider,
+  DomainGraph,
+  OpenFilesResult,
+} from 'domain-graph';
 
-import { DomainGraph, DomainGraphProps } from 'domain-graph';
+export const App: React.VFC = () => {
+  const handleDrop = useCallback(async () => {
+    return true;
+  }, []);
 
-export const App: React.VFC<{}> = () => {
-  const [schema, setSchema] = useState<Schema>();
+  const handleShowOpenDialog = useCallback(async () => {
+    return (
+      openFileDialog.current?.open() ||
+      Promise.resolve({ canceled: true, files: [] })
+    );
+  }, []);
 
-  const [dropReady, setDropReady] = useState(false);
-
-  const handleDragOver = useCallback(
-    (event: React.DragEvent<HTMLDivElement>) => {
-      event.stopPropagation();
-      event.preventDefault();
-    },
-    [],
-  );
-
-  const handleDrop = useCallback(
-    async (event: React.DragEvent<HTMLDivElement>) => {
-      // Prevent default behavior (Prevent file from being opened)
-      event.preventDefault();
-
-      const file = event.dataTransfer.files[0];
-
-      const arrayBuffer = await file.arrayBuffer();
-
-      const text = new TextDecoder().decode(arrayBuffer);
-
-      setSchema(JSON.parse(text));
-    },
-    [],
-  );
-
-  if (schema) return <DomainGraph schema={schema} />;
+  const openFileDialog = useRef<{ open: () => Promise<OpenFilesResult> }>(null);
 
   return (
-    <div
-      className={`c-uploader${dropReady ? ' drop-ready' : ''}`}
-      onDragOver={handleDragOver}
-      onDragEnter={() => setDropReady(true)}
-      onDragLeave={() => setDropReady(false)}
-      onDrop={handleDrop}
-    >
-      <UploadCloud size={200} strokeWidth={8} />
-      <h1>Drop a schema file here to get started!</h1>
-
-      <p>
-        To get a schema file, run the Apollo introspection query. Save the
-        results and drag the file into this box.
-      </p>
-    </div>
+    <>
+      <DataProvider onDrop={handleDrop} onShowOpenDialog={handleShowOpenDialog}>
+        {(introspection) => <DomainGraph introspection={introspection} />}
+      </DataProvider>
+      <BrowserOpenFileDialog
+        ref={openFileDialog}
+        accept=".json,.gql,.graphql"
+        multiple
+      />
+    </>
   );
 };
