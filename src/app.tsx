ccbf651e3ca40f317a5bcc878e6eb2e4a5732bf7
Steve Konves <stephen.konves@gmail.com>
2021-12-06 08:08:04 -0700

    feat: implement passing schema and state in URL

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,35 @@
 import './app.less';
 
-import React, { useCallback, useRef } from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
+import { useLocation } from 'react-router-dom';
+import { useFetch } from 'use-http';
+import { DocumentNode, parse } from 'graphql';
+
+import { icon } from 'domain-graph/lib/icons/base';
+
+const Loading = icon(
+  'Loading',
+  <g>
+    <line x1="12" y1="2" x2="12" y2="6" />
+    <line x1="12" y1="18" x2="12" y2="22" />
+    <line x1="4.93" y1="4.93" x2="7.76" y2="7.76" />
+    <line x1="16.24" y1="16.24" x2="19.07" y2="19.07" />
+    <line x1="2" y1="12" x2="6" y2="12" />
+    <line x1="18" y1="12" x2="22" y2="12" />
+    <line x1="4.93" y1="19.07" x2="7.76" y2="16.24" />
+    <line x1="16.24" y1="7.76" x2="19.07" y2="4.93" />
+
+    <animateTransform
+      attributeName="transform"
+      attributeType="XML"
+      type="rotate"
+      from="0 12 12"
+      to="360 12 12"
+      dur="3s"
+      repeatCount="indefinite"
+    />
+  </g>,
+);
 
 import {
   BrowserOpenFileDialog,
@@ -8,11 +37,64 @@ import {
   DomainGraph,
   OpenFilesResult,
   LocalStorageStateRepository,
+  SaveState,
 } from 'domain-graph';
 
 const repository = new LocalStorageStateRepository();
 
+const useQueryParam = (name: string): string | undefined => {
+  const { search } = useLocation();
+  return new URLSearchParams(search).get(name) || undefined;
+};
+
+const useSchema = (): {
+  loading: boolean;
+  documentNode?: DocumentNode;
+  graphId?: string;
+} => {
+  const graphId = useQueryParam('schema');
+  const { loading, error, data } = useFetch(
+    `.netlify/functions/get-schema?url=${graphId}`,
+    {},
+    [],
+  );
+
+  if (!graphId) return { loading: false };
+
+  if (loading || error) return { loading, graphId };
+
+  try {
+    return { loading, documentNode: parse(data.schema), graphId };
+  } catch (err) {
+    console.error(err);
+    return { loading, graphId };
+  }
+};
+
+const useSaveState = (): SaveState | null => {
+  const state = useQueryParam('state');
+
+  if (!state) return null;
+
+  try {
+    return JSON.parse(atob(state));
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+};
+
 export const App: React.VFC = () => {
+  const { loading, documentNode, graphId } = useSchema();
+  const saveStateFromUrl = useSaveState();
+  const { pathname, search } = useLocation();
+
+  useEffect(() => {
+    if (graphId && saveStateFromUrl) {
+      repository.set(graphId, saveStateFromUrl);
+    }
+  }, [graphId, saveStateFromUrl]);
+
   const handleDrop = useCallback(async () => {
     return true;
   }, []);
@@ -24,15 +106,61 @@ export const App: React.VFC = () => {
     );
   }, []);
 
+  const handleSaveState = useCallback(
+    (id: string, saveState: SaveState) => {
+      if (id === graphId) {
+        const newSearch = new URLSearchParams(search);
+
+        try {
+          if (Object.keys(saveState.graph.visibleNodes).length) {
+            const state = btoa(JSON.stringify(saveState));
+            newSearch.set('state', state);
+          } else {
+            newSearch.delete('state');
+          }
+        } catch (err) {
+          console.error(err);
+          newSearch.delete('state');
+        }
+
+        const newLocation = `${pathname}?${newSearch.toString()}`;
+
+        // Intentionally don't use react router's useNavigate.
+        // We don't want to trigger a rerender and we don't care if the component doesn't know about the most up-to-date state param in the query
+        window.history.replaceState(null, 'New Page Title', newLocation);
+      }
+    },
+    [graphId, search, pathname],
+  );
+
   const openFileDialog = useRef<{ open: () => Promise<OpenFilesResult> }>(null);
+
+  if (loading) {
+    return (
+      <div className="c-uploader">
+        <Loading /> Loading schema from <pre>{graphId}</pre>
+      </div>
+    );
+  }
+
+  if (documentNode && graphId) {
+    return (
+      <DomainGraph
+        graphId={graphId}
+        documentNode={documentNode}
+        repository={repository}
+        onSaveState={handleSaveState}
+      />
+    );
+  }
 
   return (
     <>
       <DataProvider onDrop={handleDrop} onShowOpenDialog={handleShowOpenDialog}>
-        {(documentNode) => (
+        {(providedNode) => (
           <DomainGraph
             graphId="default"
-            documentNode={documentNode}
+            documentNode={providedNode}
             repository={repository}
           />
         )}
