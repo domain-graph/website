ae4bcdb74032e610c7ca1a2c80e4db504b73f32a
Steve Konves <stephen.konves@gmail.com>
2021-12-18 14:35:31 -0700

    feat: configure google analytics

--- src/pages/router.tsx
+++ src/pages/router.tsx
@@ -1,11 +1,26 @@
-import React, { useContext, useMemo, useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useContext, useEffect, useMemo, useState } from 'react';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import { App } from './app';
 import { Blog } from './blog';
 import { Docs } from './docs';
 import { Home } from './home';
 
 import { DocumentNode } from 'graphql';
+
+import Analytics from 'analytics';
+import { AnalyticsProvider } from 'use-analytics';
+import doNotTrack from 'analytics-plugin-do-not-track';
+import googleAnalytics from '@analytics/google-analytics';
+
+const analytics = Analytics({
+  app: 'domain-graph',
+  plugins: [
+    doNotTrack(),
+    googleAnalytics({
+      trackingId: 'UA-215646920-1',
+    }),
+  ],
+});
 
 export type AppState = {
   graphId?: string;
@@ -37,15 +52,28 @@ export const Router = () => {
   );
 
   return (
-    <AppContext.Provider value={value}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="app/*" element={<App />} />
-          <Route path="blog/*" element={<Blog />} />
-          <Route path="docs/*" element={<Docs />} />
-        </Routes>
-      </BrowserRouter>
-    </AppContext.Provider>
+    <AnalyticsProvider instance={analytics}>
+      <AppContext.Provider value={value}>
+        <BrowserRouter>
+          <NavigationHandler />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="app/*" element={<App />} />
+            <Route path="blog/*" element={<Blog />} />
+            <Route path="docs/*" element={<Docs />} />
+          </Routes>
+        </BrowserRouter>
+      </AppContext.Provider>
+    </AnalyticsProvider>
   );
+};
+
+const NavigationHandler: React.VFC = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    analytics.page();
+  }, [location.pathname, location.search, location.hash]);
+
+  return null;
 };
