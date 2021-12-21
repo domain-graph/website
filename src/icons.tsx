642a67960b89ea958ccf7b4673e0904b5e557e05
Steve Konves <stephen.konves@gmail.com>
2021-12-21 10:32:15 -0700

    feat: make it look good on mobile

--- src/icons.tsx
+++ src/icons.tsx
@@ -40,3 +40,12 @@ export const Loading = icon(
     />
   </g>,
 );
+
+export const Menu = icon(
+  'Menu',
+  <>
+    <line x1="3" y1="12" x2="21" y2="12"></line>
+    <line x1="3" y1="6" x2="21" y2="6"></line>
+    <line x1="3" y1="18" x2="21" y2="18"></line>
+  </>,
+);
--- src/pages/template.tsx
+++ src/pages/template.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Menu } from '../icons';
 import { Logo } from './logo';
 
 export const HeaderLink: React.FC<{
@@ -8,45 +9,51 @@ export const HeaderLink: React.FC<{
 }> = ({ children, to, external }) => (
   <li>
     {external ? (
-      <a className="p-8" href={to}>
+      <a className="p-2 md:p-6" href={to}>
         {children}
       </a>
     ) : (
-      <Link className="p-8" to={to}>
+      <Link className="p-2 md:p-6" to={to}>
         {children}
       </Link>
     )}
   </li>
 );
 
-export const Template: React.FC = ({ children }) => (
-  <div>
-    <div className="sticky top-0 bg-eggshell flex items-center justify-between shadow">
-      <Link to="/" className="flex items-center p-2">
-        <Logo className="w-12 h-12 m-4" />
-        <h1 className="text-2xl">Domain Graph</h1>
-      </Link>
+export const Template: React.FC = ({ children }) => {
+  const [show, setShow] = useState(false);
+  const handleClick = useCallback(() => setShow(!show), [show]);
+  return (
+    <div>
+      <div className="shadow bg-eggshell md:sticky md:top-0 md:flex md:justify-between md:items-center">
+        <div className="flex justify-between">
+          <Link to="/" className="flex items-center p-2">
+            <Logo className="w-12 h-12 m-4" />
+            <h1 className="text-2xl">Domain Graph</h1>
+          </Link>
+          <button className="md:hidden p-8" onClick={handleClick}>
+            <Menu />
+          </button>
+        </div>
 
-      <nav>
-        <ul className="flex">
-          {/* <HeaderLink to="/app">Try it out!</HeaderLink>
-          <HeaderLink to="/docs">Documentation</HeaderLink>
-          <HeaderLink to="/blog">Blog</HeaderLink> */}
-          <HeaderLink to="https://github.com/sponsors/skonves" external>
-            ❤️ Sponsor
-          </HeaderLink>
-          <HeaderLink
-            to="https://marketplace.visualstudio.com/items?itemName=stevekonves.domain-graph-vscode"
-            external
-          >
-            VS Code extension
-          </HeaderLink>
-          <HeaderLink to="https://github.com/domain-graph" external>
-            Github
-          </HeaderLink>
-        </ul>
-      </nav>
+        <nav className={`${show ? '' : 'hidden'} md:block text-center text-xl pb-4 md:pb-0`}>
+          <ul className="md:flex">
+            <HeaderLink to="https://github.com/sponsors/skonves" external>
+              ❤️ Sponsor
+            </HeaderLink>
+            <HeaderLink
+              to="https://marketplace.visualstudio.com/items?itemName=stevekonves.domain-graph-vscode"
+              external
+            >
+              VS Code extension
+            </HeaderLink>
+            <HeaderLink to="https://github.com/domain-graph" external>
+              Github
+            </HeaderLink>
+          </ul>
+        </nav>
+      </div>
+      <section className="flex justify-center flex-col">{children}</section>
     </div>
-    <section className="flex justify-center flex-col">{children}</section>
-  </div>
-);
+  );
+};
